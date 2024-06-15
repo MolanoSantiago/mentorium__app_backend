d64959e525f9f6af1a9d5c91cffe002c61cf0142
MolanoSantiago <s.molano0818@gmail.com>
2024-06-15 12:19:36 -0500

    Add signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,21 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Constants\TokenAbilityEnum;
+use App\Http\Requests\AuthRequest;
 use App\Http\Resources\AuthResource;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 
 class AuthController extends Controller
 {
-    public function login(Request $request): JsonResponse
+    public function login(AuthRequest $request): JsonResponse
     {
-        $credentials = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+        $credentials = $request->only('email', 'phone', 'password');
 
-        if (auth()->attempt($credentials)) {
+        $loginField = !empty($credentials['email']) ? 'email' : 'phone';
+        $loginValue = $credentials[$loginField];
+
+        if (auth()->attempt([$loginField => $loginValue, 'password' => $credentials['password']])) {
             $user = auth()->user();
 
             $accessToken = $user->createToken(
@@ -68,5 +70,30 @@ class AuthController extends Controller
             'access_token' => $accessToken->plainTextToken,
             'token_type' => 'Bearer'
         ])->response()->setStatusCode(200);
+    }
+
+    public function signup(AuthRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        $user = User::create($data);
+
+        $accessToken = $user->createToken(
+            'access_token',
+            [TokenAbilityEnum::ACCESS_API->value],
+            Carbon::now()->addMinutes((int)config('sanctum.access_token_expiration'))
+        );
+
+        $refreshToken = $user->createToken(
+            'refresh_token',
+            [TokenAbilityEnum::ISSUE_ACCESS_TOKEN->value],
+            Carbon::now()->addMinutes((int)config('sanctum.refresh_token_expiration'))
+        );
+
+        return AuthResource::make($user)->additional([
+            'access_token' => $accessToken->plainTextToken,
+            'refresh_token' => $refreshToken->plainTextToken,
+            'token_type' => 'Bearer'
+        ])->response()->setStatusCode(201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,16 @@ use App\Constants\TokenAbilityEnum;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('/refresh-token', [AuthController::class, 'refresh'])
-        ->middleware('ability:' . TokenAbilityEnum::ISSUE_ACCESS_TOKEN->value)
-        ->name('refresh-token');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login')->name('login');
+    Route::post('/signup', 'signup')->name('signup');
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/refresh-token', 'refresh')
+            ->middleware('ability:' . TokenAbilityEnum::ISSUE_ACCESS_TOKEN->value)
+            ->name('refresh-token');
+        Route::get('/logout', 'logout')
+            ->middleware('ability:' . TokenAbilityEnum::ACCESS_API->value)
+            ->name('logout');
+    });
 });
